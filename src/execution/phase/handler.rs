1b88118334287630a4f472a785081ce90791faf2
k-ogata <ogtkzk712@gmail.com>
2026-06-21 22:04:46 +0900

    mod: source phase中に登録した即時イベントはその時刻が始まったタイミングではまだ登録されていなかったイベントなので、直後のevent phaseですぐにフラッシュされないように修正

--- src/execution/phase/handler.rs
+++ src/execution/phase/handler.rs
@@ -31,7 +31,6 @@ impl<E, M: Model<E>> MicroStepHandler<ActiveExecutorContext<E, M>> {
             context.current_tick_status.current(),
             context.next_micro_step_status.current(),
         );
-        context.source_handler.flush_pending();
 
         let ready_sources = context
             .source_handler
@@ -58,7 +57,6 @@ impl<E, M: Model<E>> MicroStepHandler<SourceContext<E, M>> {
             context.current_tick_status.current(),
             context.current_micro_step_status.current(),
         );
-        context.event_scheduler.flush_pending();
 
         let ready_events = context
             .event_scheduler
